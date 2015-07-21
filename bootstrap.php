7f9982ff01aa14e796463fd65cbe3e40434727d7
luzhongpeng <1234qwqw>
2015-07-21 23:30:59 +0800

    init

---
+++ bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+
+use tourze\Base\Config;
+use tourze\Base\I18n;
+use tourze\Base\Message;
+use tourze\View\View;
+
+// 指定配置加载目录
+Config::addPath(__DIR__ . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR);
+
+// 语言文件目录
+I18n::addPath(__DIR__ . DIRECTORY_SEPARATOR . 'i18n' . DIRECTORY_SEPARATOR);
+
+// Message目录
+Message::addPath(__DIR__ . DIRECTORY_SEPARATOR . 'message' . DIRECTORY_SEPARATOR);
+
+// 指定视图加载目录
+View::addPath(__DIR__ . DIRECTORY_SEPARATOR . 'view' . DIRECTORY_SEPARATOR);
